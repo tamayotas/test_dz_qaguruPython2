79fa93323d1b0517ce18cb05d114ec8bbc8d55dd
tamayotas <tamaevs@gmail.com>
2023-12-21 22:00:24 +0300

    Повторение доп занятия с Иваном по Пайтон часть 2
    Решение задачек от Алексея

--- test_dzpython_2.py
+++ test_dzpython_2.py
@@ -36,28 +36,6 @@ def find_registration_button_on_login_page(page_url, button_text):
                              " Register]")
 
 
-def sum_result(a, b):
-    result1 = a
-    result2 = b
-    assert (result2 + result1)
-    print()
-
-# функция, которая принмает в себя число и возвращает true если оно чётное и false если не чётное
-
-def is_odd(num):
-    # если число чётное то возращает true
-    if num % 2 == 0:
-        print("true")
-    # и false если не чётное
-    if num % 2 != 0:
-        print("false")
-
-#  проверить типы левой и правой части assert
-num = 3
-step1 = num % 2
-step2 = 0
-print (type(step1))
-print (type(step2))
 
 
 
