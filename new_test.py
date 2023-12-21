05e26aad8a418f3f1bb77d5f5fd46e90d733c95f
tamayotas <tamaevs@gmail.com>
2023-12-21 22:01:06 +0300

    Повторение доп занятия с Иваном по Пайтон часть 2
    Решение задачек от Алексея

--- new_test.py
+++ new_test.py
@@ -1,15 +1,19 @@
 
 # функция, которая принмает в себя число и возвращает true если оно чётное и false если не чётное
 def is_odd(num):
+
     # если число чётное то возращает true
     if num % 2 == 0:
-        print("true")
+        return True
     # и false если не чётное
     if num % 2 != 0:
-        print("false")
-
+        return False
+# назначено значение 3 для переменной num
 num = 3
-is_odd(num)
+x = is_odd(num)
+y = False
+print(x)
+print(y)
 
-assert is_odd(num) == "false"
+assert x == y
 
