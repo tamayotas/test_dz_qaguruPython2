e14cda010694af37959498094d0ffa8b5a1ce0cf
tamayotas <tamaevs@gmail.com>
2024-01-04 12:48:20 +0300

    Повторение доп занятия с Иваном по Пайтон часть 2
    Решение задачек от Алексея
    
    Добавлены недостающие тесты

--- test_dzpython_2.py
+++ test_dzpython_2.py
@@ -1,4 +1,75 @@
 from new_test import num
+from datetime import time
+
+
+def test_dark_theme_by_time():
+    """
+    Протестируйте правильность переключения темной темы на сайте в зависимости от времени
+    """
+    current_time = time(hour=23)
+
+    if 6 < current_time.hour < 22:
+        is_dark_theme = False
+    else:
+        is_dark_theme = True
+    assert is_dark_theme is True
+
+
+def test_dark_theme_by_time_and_user_choice():
+    """
+    Протестируйте правильность переключения темной темы на сайте
+    в зависимости от времени и выбора пользователя
+    dark_theme_enabled_by_user = True - Темная тема включена
+    dark_theme_enabled_by_user = False - Темная тема выключена
+    dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
+    """
+    current_time = time(hour=22)
+    dark_theme_enabled_by_user = True
+
+    if dark_theme_enabled_by_user is None and (current_time.hour >= 22 or current_time.hour <= 6):
+        is_dark_theme = True
+    elif dark_theme_enabled_by_user is None and (6 < current_time.hour < 22):
+        is_dark_theme = False
+    else:
+        is_dark_theme = dark_theme_enabled_by_user
+
+    assert is_dark_theme is True
+
+
+def test_find_suitable_user():
+    """
+    Найдите нужного пользователя по условиям в списке пользователей
+    """
+    users = [
+        {"name": "Ivan", "age": 32},
+        {"name": "Sergey", "age": 24},
+        {"name": "Igor", "age": 15},
+        {"name": "Olga", "age": 45},
+        {"name": "Irina", "age": 18},
+    ]
+
+    # TODO найдите пользователя с именем "Olga"
+    suitable_users = None
+
+    for i in users:
+        if i['name'] == 'Olga':
+            suitable_users = i
+
+    assert suitable_users == {"name": "Olga", "age": 45}
+
+    # TODO найдите всех пользователей младше 20 лет
+
+    suitable_users = []
+
+    for i in users:
+        if i['age'] < 20:
+            suitable_users.append(i)
+
+    assert suitable_users == [
+        {"name": "Igor", "age": 15},
+        {"name": "Irina", "age": 18},
+    ]
+
 
 def prettify_func_name(func, *argument):
     return f'{func.__name__.replace("_", " ").title()} [{", ".join([*argument])}]'
@@ -34,6 +105,8 @@ def find_registration_button_on_login_page(page_url, button_text):
     actual_result = prettify_func_name(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == ("Find Registration Button On Login Page [https://companyname.com/login,"
                              " Register]")
+
+
 
 
 
